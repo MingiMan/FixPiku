5c653e5448f94eeb850d8ed1935e021047d596bc
ShinMinGi <minky5576@naver.com>
2024-08-13 17:19:04 +0900

    원거리 공격

---
+++ Assets/신민기/02_Scripts/Player/Bullet.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage;
+    Rigidbody rigid;
+    public float speed;
+    public float destroyTime;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        Destroy(this.gameObject, destroyTime);
+    }
+
+    public void FireBullet(Transform pos)
+    {
+        rigid.linearVelocity = pos.forward*speed;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ENEMY"))
+        {
+            other.gameObject.GetComponent<Enemys>().Damage(damage,transform.position);
+        }
+    }
+
+}
